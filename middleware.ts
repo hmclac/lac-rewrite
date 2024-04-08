dedc62f0188db83446d6101ca91692e15a56a7d6
Kai <rieffelkai@gmail.com>
2024-04-07 21:52:52 -0700

    still trying to fix

---
+++ middleware.ts
@@ -0,0 +1,13 @@
+import { withAuth } from 'next-auth/middleware';
+
+import mid from 'next-auth/middleware';
+
+export const middleware = withAuth({
+  pages: {
+    signIn: '/login',
+  },
+});
+
+export const config = {
+  matcher: ['/employee'],
+};
